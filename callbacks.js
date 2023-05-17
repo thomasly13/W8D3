09ab1384d848c573a66c21098ea76192dc348fa8
Thomas Ly <Thomaslyy13@gmail.com>
2023-05-17 13:35:24 -0700

    started the clock

---
+++ callbacks.js
@@ -0,0 +1,21 @@
+class Clock {
+    constructor() {
+        const todaysDate = new Date('May 17, 2023 12:05:00')
+        let date = todaysDate.getDate();
+        this.hours = todaysDate.getHours();
+        this.minutes = todaysDate.getMinutes();
+        this.seconds = todaysDate.getSeconds();
+
+        
+        
+      // 1. Create a Date object.
+      // 2. Store the hours, minutes, and seconds.
+      // 3. Call printTime.
+      // 4. Schedule the tick at 1 second intervals.
+    }
+
+    printTime() {
+
+    }
+
+}
